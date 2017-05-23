22b12f5bbbf65dea7234b82e44519b0b55261dcd
flouly <flouly@orange.fr>
2017-05-23 09:17:10 +0200

    fin de inscription

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connexion a la BDD
-/*$pdo = new PDO('mysql:host=localhost;dbname=sallea', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));*/
+$pdo = new PDO('mysql:host=localhost;dbname=sallea', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 // Session
 session_start();
--- inscription.php
+++ inscription.php
@@ -1,72 +1,85 @@
 <?php
 require_once('inc/init.inc.php');
 
+$inscription = false;
+//print_r($_POST);
 
 if(!empty($_POST)){
 
+            if(strlen($_POST['pseudo']) < 4 || strlen($_POST['pseudo']) > 20){
+
+                $contenu .= '<p>Le pseudo doit contenir au moins 4 caracteres<p>';
+            }
+
+             if(strlen($_POST['mdp']) < 4 || strlen($_POST['mdp']) > 60){
+
+                $contenu .= '<p>Le mot de passe doit contenir au moins 4 caracteres</p>';
+            }
+
+                      if(strlen($_POST['nom']) < 4 || strlen($_POST['nom']) > 20){
+
+                $contenu .= '<p>Le nom doit contenir au moins 4 caracteres</p>';
+            }
+
+
+             if(strlen($_POST['prenom']) < 4 || strlen($_POST['prenom']) > 20){
+
+                $contenu .= '<p>Le prenom doit contenir au moins 4 caracteres</p>';
+            }
+
+            if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+
+                $contenu .= '<p>Email est invalide </p>';
+            }
+
+              if($_POST['civilite'] != 'm' && $_POST['civilite'] != 'f'){
+                $contenu .= '<p>La civilite est incorrecte</p>';
+            }
+
+            if(empty($contenu)){
+
+
+                $membre = executeRequete("SELECT id_membre FROM membre  WHERE pseudo = :pseudo", array(':pseudo' => $_POST['pseudo']));
+
+                if($membre->rowCount() > 0){
+
+                    $contenu .= '<p>Le pseudo est deja pris</p>';
+
+                } else {
+
+                    $_POST['mdp'] = md5($_POST['mdp']);
+
+                    executeRequete("INSERT INTO membre (pseudo, mdp, nom, prenom, email, civilite, statut, date_enregistrement) VALUES (:pseudo, :mdp, :nom, :prenom, :email, :civilite, 0, NOW())", array(
+                        ':pseudo' => $_POST['pseudo'], 
+                        ':mdp' => $_POST['mdp'], 
+                        ':nom' => $_POST['nom'], 
+                        ':prenom' => $_POST['prenom'], 
+                        ':email' => $_POST['email'], 
+                        ':civilite' => $_POST['civilite']
+                        )
+                       );
+
+
+                    $contenu .= '<p>vous avez ete enregistre</p>';
+                    $inscription = true;
+                }
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
 
 
 }//fin if(!empty($_POST))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*********************Affichage****************************/
 require_once('inc/haut.php');
 
 echo $contenu;
+if(!$inscription) :
 ?>
 
     <h2>S'inscrire</h2>
@@ -77,8 +90,8 @@ echo $contenu;
         <input type="text" name="prenom" id="prenom" placeholder="Votre prenom" value="">
         <input type="text" name="email" id="email" placeholder="Votre email" value="">
         <select name="civilite" id="civilite">
-            <option value="homme">Homme</option>
-            <option value="femme">Femme</option>
+            <option value="m">Homme</option>
+            <option value="f">Femme</option>
         </select>
 
         <input type="submit" value="inscription">
@@ -86,9 +99,8 @@ echo $contenu;
     </form>
 
     
-</body>
-</html>
+
 
 <?php
-
+endif;
 require_once('inc/bas.php');
